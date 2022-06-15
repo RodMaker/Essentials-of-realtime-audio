3b3e8fd5a817adedcafc03755d2c710306f51002
RodMaker <83619630+RodMaker@users.noreply.github.com>
2022-06-15 10:36:01 +0100

    Version15062022

---
+++ Assets/MoveChicken.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveChicken : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    //void OnTriggerEnter()
+    //{
+        //object gameObject = GameObject.Active;
+    //}
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
